4fb5094fd696735d167a27d0cf8527d48fdb29bc
jbneto <jbneto.ep@gmail.com>
2022-11-01 20:09:32 -0300

    Organizei pacotes de exercicios

---
+++ Anatomia-de-classes/anatomia-de-classes1/src/edu/exercicio/primeiro/AnatomiaClasse1.java
@@ -0,0 +1,14 @@
+package edu.exercicio.primeiro;
+public class AnatomiaClasse1 {
+
+public static void main (String[] args) {
+ String primeiroNome = "José";
+ String segundoNome = "Neto";
+
+ String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.print(nomeCompleto);
+}
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome);
+} 
+}
