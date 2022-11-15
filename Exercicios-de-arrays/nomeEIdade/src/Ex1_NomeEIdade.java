517d6387d509352b5e1a8df9b5830b41589fba9c
jbneto <jbneto.ep@gmail.com>
2022-11-15 11:41:36 -0300

    criando arquivo exercicio loop

---
+++ Exercicios-de-arrays/nomeEIdade/src/Ex1_NomeEIdade.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+              String nome;
+              int idade;
+   
+              System.out.println("Nome: ");
+              nome = scan.next();
+              System.out.println("Idade: ");
+              idade = scan.nextInt();
+            }
+      
+         }
+
