03e2b6487747cae7fedf02a4d7e0403cb04daae9
jbneto <jbneto.ep@gmail.com>
2022-11-18 19:47:20 -0300

    Criei exemplo de medias

---
+++ Exercicios-de-arrays/nota/Ex3_MaiorEMedia/Ex3_MaiorEMedia/src/Ex3_MaiorEMedia.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex3_MaiorEMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int numero;
+        int maior = 0;
+        int soma = 0;
+        int count = 0;
+        do{
+        System.out.println("Numero: ");
+        numero = scan.nextInt();
+        
+        soma = soma + numero;
+        
+        if (numero>maior)maior=numero;
+        count++;
+        System.out.println("Maior: " + maior);
+        }while(count<5);
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Media: " + (soma/5));
+    }
+}
