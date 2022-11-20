8b3c7b2c5467821abe0725887393506f9bfbfdfd
jbneto <jbneto.ep@gmail.com>
2022-11-20 19:43:12 -0300

    criando arquivos consoantes

--- Exercicios-de-loops-e-arrays/Arrays/consoantes/src/Ex2_Consoantes.java
+++ Exercicios-de-loops-e-arrays/Arrays/consoantes/src/Ex2_Consoantes.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Ex2_Consoantes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        String[] consoantes = new String[6]; 
+        int quantidadeConsoantes = 0;   
+        int count = 0;
+        
+        do {
+            System.out.println("Letra: ");
+            String letra = scan.next();
+
+            if ( !(letra.equalsIgnoreCase("a")| 
+            letra.equalsIgnoreCase("e")|
+            letra.equalsIgnoreCase("i")|
+            letra.equalsIgnoreCase("o")|
+            letra.equalsIgnoreCase("u")));{
+        consoantes[count] = letra;
+        quantidadeConsoantes++;
+            }
+            count++;
+
+        } while(count<consoantes.length);
+
+        System.out.println("Consoantes: ");
+        for (String consoante : consoantes) {
+                     
+            System.out.println(consoante + " ");
+        }
     }
 }
