5d5747222d0870c2113806a9878b58864ce3b358
jbneto <jbneto.ep@gmail.com>
2022-11-22 20:11:30 -0300

    criei arquivo mensagem e adicionei metodo ao main

--- Exercicios-de-metodos/src/Main.java
+++ Exercicios-de-metodos/src/Main.java
@@ -7,5 +7,12 @@ public class Main {
     Calculadora.subtracao(1, 8);
     Calculadora.multiplicacao(7, 8);
     Calculadora.divisao(5, 2.5);
+
+    //Mensagem
+    System.out.println("Exercicio mensagem");
+    Mensagem.obterMensagem(9);
+    Mensagem.obterMensagem(14);
+    Mensagem.obterMensagem(1);
+
     }
 }
