14be5adce35b13dcca1a399d26ef2ecddc4de4de
José Benedito dos Santos Neto <jbneto.ep@gmail.com>
2023-01-23 20:47:11 -0300

    criando exercicio stream api

---
+++ Exercicio-map-stream/src/ExerciciosStreamAPI.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ExerciciosStreamAPI {
+    public static void main(String[] args) {
+        List<String> numerosAleatorios =
+                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
+
+        System.out.println("Imprima todos os elementos dessa lista de String: ");
+        //numerosAleatorios.stream().forEach(new Consumer<String>() {
+        //  @Override
+        //public void accept(String s) {
+        //  System.out.println(s);
+        numerosAleatorios.forEach(System.out::println);
+
+        System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set:");
+        numerosAleatorios.stream()
+                .limit(5)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        System.out.println("Transforme esta lista de String em uma lista de números inteiros.");
+        numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+}
