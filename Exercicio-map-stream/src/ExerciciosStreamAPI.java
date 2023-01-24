578214fe722d45a00ebb4c8ff3e85776a5587e2d
José Benedito dos Santos Neto <jbneto.ep@gmail.com>
2023-01-23 21:37:52 -0300

    transformando imports nao usados em comentarios

--- Exercicio-map-stream/src/ExerciciosStreamAPI.java
+++ Exercicio-map-stream/src/ExerciciosStreamAPI.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
+//import java.util.function.Consumer;
+//import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class ExerciciosStreamAPI {
