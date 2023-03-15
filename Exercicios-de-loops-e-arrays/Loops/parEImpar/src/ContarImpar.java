10d6ca442cee953cfa4f50343f1d9fb6e061ce47
José Benedito dos Santos Neto <jbneto.ep@gmail.com>
2023-03-15 20:48:13 -0300

    melhorando codigo

--- Exercicios-de-loops-e-arrays/Loops/parEImpar/src/ContarImpar.java
+++ Exercicios-de-loops-e-arrays/Loops/parEImpar/src/ContarImpar.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 public class ContarImpar {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        
         int pessoasNoCamarote = 0;
-
-       // System.out.println("Digite a quantidade de pessoas na fila: ");
         int tamanhoDaFila = leitor.nextInt();
         
         // TODO: Crie a condição necessária para verificar quais números, de acordo com o range de entrada, são ímpares.
